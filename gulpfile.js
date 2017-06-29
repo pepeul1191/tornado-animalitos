996442fb5f30c5af0cc879a5fc74dfafd6dd1209
Pepe Valdivia <jvaldivia@softweb.pe>
2017-06-29 18:06:43 -0500

    El gulp está a la escucha de cambios para realizar sus tasks

--- gulpfile.js
+++ gulpfile.js
@@ -4,9 +4,20 @@ var concatCss = require('gulp-concat-css');
 var concatJs = require("gulp-concat");
 var minifyCss = require("gulp-minify-css");
 var replace = require('gulp-replace');
+var plumber = require('gulp-plumber');
+var watch = require('gulp-watch');
 var BASE_URL = 'http://localhost:8888/static/';
 
 // --------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+function errorLog(error){
+    console.error.bind(error);
+    this.emit('end');
+}
+
+gulp.task('watch', function(){
+    gulp.watch(['media/assets/login/css/index.css'], ['login']);
+});
 
 gulp.task('default', function(){
 	console.log("HOLA!!!");
@@ -14,6 +25,7 @@ gulp.task('default', function(){
 
 gulp.task('demo', function(){
   gulp.src('assets/*/*.js')
+    .pipe(plumber())
     .pipe(uglify())
     .pipe(gulp.dest('media/dist/js'));
 });
@@ -22,19 +34,22 @@ gulp.task('demo', function(){
 
 gulp.task('fonts', function() {
   	gulp.src(['media/bower_components/font-awesome/fonts/*', 'media/bower_components/bootstrap/fonts/*'])
+    .pipe(plumber())
       .pipe(gulp.dest('media/dist/assets'));
 });
 
 gulp.task('layout-css', function() {
       gulp.src(['media/dist/assets/icons.min.css','media/bower_components/bootstrap/dist/css/bootstrap.min.css', 'media/bower_components/font-awesome/css/font-awesome.min.css', 'media/bower_components/unify-pp/css/styles.css'])
+      .pipe(plumber())
       .pipe(concatCss('styles.min.css'))
       .pipe(minifyCss())
-      .pipe(replace('../../../font-awesome/fonts/', 'http://localhost:8888/static/dist/assets/'))
+      .pipe(replace('../../../font-awesome/fonts/', BASE_URL + 'dist/assets/'))
       .pipe(gulp.dest('media/dist/assets'));
 });
 
 gulp.task('layout-js', function() {
     gulp.src(['media/bower_components/jquery/dist/jquery.min.js', 'media/bower_components/bootstrap/dist/js/bootstrap.min.js', 'media/bower_components/underscore/underscore-min.js', 'media/bower_components/handlebars/handlebars.min.js', 'media/bower_components/swp-plugins/assets/js/mootools-core.min.js', 'media/bower_components/swp-plugins/assets/js/mootools.min.js', 'media/bower_components/swp-plugins/assets/js/mootools-interfaces.min.js'])
+    .pipe(plumber())
     .pipe(concatJs('app.min.js'))  // concat and name it "concat.js"
     .pipe(gulp.dest('media/dist/assets'));
 });
@@ -45,7 +60,10 @@ gulp.task('layout', ['fonts', 'layout-css', 'layout-js']);
 
 gulp.task('login', function(){
   gulp.src(['media/dist/assets/styles.min.css','media/assets/login/css/index.css'])
+    .pipe(plumber())
     .pipe(concatCss('styles.min.css'))
     .pipe(minifyCss())
+    .on('error', errorLog)
     .pipe(gulp.dest('media/dist/login'));
 });
+
